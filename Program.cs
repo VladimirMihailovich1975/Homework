0973a69863b14ac5110acf6797a20542c8cd57e5
unknown <vsarvas32@gmail.com>
2023-03-18 23:40:16 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,24 +1,2 @@
 ﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "32"};
 string[] array2 = new string[array1.Length];
-void SecArray(string[] array1, string[] array2)
-{
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
-    {
-    if(array1[i].Length <= 3)
-        {
-        array2[count] = array1[i];
-        count++;
-        }
-    }
-}
-void Print(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-SecArray(array1, array2);
-Print(array2);
