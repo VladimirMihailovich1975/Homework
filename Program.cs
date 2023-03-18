8edb0b99ee3ed0c7cce3c45a8dff71cd2be62f00
unknown <vsarvas32@gmail.com>
2023-03-18 23:41:25 +0300

    4

--- Program.cs
+++ Program.cs
@@ -12,3 +12,13 @@ void SecArray(string[] array1, string[] array2)
         }
     }
 }
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecArray(array1, array2);
+Print(array2);
